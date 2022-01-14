00863cea730fc57d612039eeaab1ce471419e581
Shane Nicholson <shanescottnicholson@gmail.com>
2022-01-13 16:37:05 -0800

    Adds fib problem

--- practice.rb
+++ practice.rb
@@ -10,11 +10,48 @@
 
 # practice(9)
 
-def hello(string)
-  p "Hello, my name is #{string}."
+# def hello(string)
+#   p "Hello, my name is #{string}."
+# end
+
+# p "Enter your name."
+# var = gets.chomp
+
+# hello(var)
+
+# name = "shane"
+
+# p name.upcase
+# p name.reverse
+
+# def calculate(number, times)
+
+#   number = number * times
+
+#   return number
+
+# end
+
+# p calculate(12, 4)
+
+def fib(num)
+
+  fib = [0, 1]
+
+  if num <= 2
+    return fib[num - 1]
+  end
+
+  (num - 2).times do 
+    index = fib.length
+    hold_one = fib[index - 1]
+    hold_two = fib[index - 2]
+    nex = hold_one.to_i + hold_two.to_i
+    fib << nex
+  end
+
+  return fib, fib[num - 1]
+    
 end
 
-p "Enter your name."
-var = gets.chomp
-
-hello(var)
+p fib(23)
