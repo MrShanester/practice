85225f099a576580fa1b459e272f1c61a71605c5
Shane Nicholson <shanescottnicholson@gmail.com>
2022-02-04 17:35:04 -0800

    Adds longest non repeating substring problem

--- practice.rb
+++ practice.rb
@@ -600,8 +600,36 @@
 # p happy?(2)
 
 # wrote this
-def generate(num_rows)
-  num_rows.times.map{|n|[p=1]+(1..n).map{|k|p=p*(n-k+1)/k}}
+# def generate(num_rows)
+#   num_rows.times.map{|n|[p=1]+(1..n).map{|k|p=p*(n-k+1)/k}}
+# end
+
+# p generate(5)
+
+# launchpad labs indeed job? Daniel Schadd
+
+# return length of longest non repeating substring
+
+# input = "abcabcbba"
+# output = 3
+
+def longest_sub(input)
+  i = 1
+  val = 1
+  hash = {input[0] => true}
+  output = 0
+  while i < input.length
+    if hash[input[i]]
+      output = val if val > output
+      val = 0
+      i += 1
+    else
+      hash[input[i]] = true
+      val += 1
+      i += 1
+    end
+  end
+  output
 end
 
-p generate(5)
+p longest_sub("bbbbbbbbb")
