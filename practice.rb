9ab7b264a3fe4e414cbf1ce2c9c62e01245b67d2
Shane Nicholson <shanescottnicholson@gmail.com>
2022-03-07 09:39:49 -0800

    Adds different methods for finding the longest word in a phrase

--- practice.rb
+++ practice.rb
@@ -946,11 +946,28 @@
 
 # opinion()
 
-require 'faker'
+# require 'faker'
 
-10.times do
-  puts Faker::Name.name
+# 10.times do
+#   puts Faker::Name.name
+# end
+
+# def find_longest_word(sentence)
+#   split_sentence = sentence.split(" ")
+#   longest = split_sentence.max_by(&:length)
+#   p longest 
+# end 
+
+# find_longest_word("Write a method called find_longest_word, which will accept a string as a parameter")
+
+def find_longest_word(string)
+  separate = string.split(" ")
+  separate.sort_by! { |separate| separate.size }
+  answer = separate.reverse
+  answer[0]
 end
+
+p find_longest_word("What is the word in this phrase? longest")
 
 
 
